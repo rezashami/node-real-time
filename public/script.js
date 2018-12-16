58d1eb5cdc615ccc0ad1123692a3ad6515f15d83
Reza Shami Tanha <reza.shami.tanha@gmail.com>
2018-12-16 19:47:36 +0330

    Last Change

--- public/script.js
+++ public/script.js
@@ -2,4 +2,11 @@ var socket = io();
 socket.on('connect',()=>{
     console.log('Connected to server');
 });
+socket.on('login',function(users){
+    console.log(users);
+});
+
+socket.on('new message',function(message){
+    console.log(message);
+});
 socket.on('disconnect',()=>{console.log('Disconnected form server');});
